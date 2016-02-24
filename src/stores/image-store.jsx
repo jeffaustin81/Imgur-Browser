f3ecfc0ebd86d394eb1368c8bbae6231d129f373
Jeff Austin <jeffaustin81@gmail.com>
2016-02-24 10:03:29 -0800

    Add utils to filter images/videos by height and width to improve display

--- src/stores/image-store.jsx
+++ src/stores/image-store.jsx
@@ -9,6 +9,12 @@ module.exports = Reflux.createStore({
     Api.get('topics/' + topicId)
       .then(function(json){
         this.images = _.reject(json.data, function(image) {
+          if(image.height > 600) {
+            return image.height
+          }
+          if(image.width > 600) {
+            return image.width
+          }
           return image.is_album
         });
         this.triggerChange();
